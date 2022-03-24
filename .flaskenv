74d5e54ff8281a53bba46ca71842a7ae1b67d3c6
vbankovic <vbankovic@rechargeapps.com>
2022-03-24 11:29:18 +0100

    first commit

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP=my_blog.py
---
+++ my_blog.py
@@ -0,0 +1,10 @@
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
