6b137b08a849b126fa96ecef3b32b8981f216ea9
zorzbraun <vladimir.bankovic1992@gmail.com>
2022-10-03 10:37:51 +0200

    Adding some changes

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
-from app import app, db
+from app import db
 from app.main.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User, Post
 from app.email import send_password_reset_email
